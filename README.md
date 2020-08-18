e19dc34eb239434a3acf65776a8a12e5f63e61cb
Edilson Borges <edilson.borges@agrodefesa.go.gov.br>
2020-08-17 22:44:09 -0300

    flutter crud

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sandbox-flutter
 Some own example projects using stacks like flutter, firebase, etc
+
+## 1 - flutter_crud
+Application for user management. View, sign Up, sign In, edit and delete.
---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Rogério',
+      email: 'rogerio@teste.com',
+      avatarUrl:
+          'https://cdn.iconscout.com/icon/free/png-512/avatar-370-456322.png'),
+  '2': const User(
+      id: '2',
+      name: 'Carla',
+      email: 'carla@teste.com',
+      avatarUrl:
+          'https://cdn1.iconfinder.com/data/icons/avatar-97/32/avatar-02-512.png'),
+  '3': const User(
+      id: '3',
+      name: 'Marcos',
+      email: 'marcos@teste.com',
+      avatarUrl: 'https://i.dlpng.com/static/png/7105396_preview.png'),
+};
